bd5a0f864f72dd6983fc5cacc03ede4f1598da83
MatiasMastrangelo <mrmastra@hotmail.com>
2024-04-21 22:46:30 -0300

    git ignore

--- analytic_account_auto/__init__.py
+++ analytic_account_auto/__init__.py
@@ -1,1 +1,1 @@
-from .models import analytic_account_auto, analytic_account, analytic_distribution_model, account_move_line
+from .models import analytic_account_auto, analytic_account, analytic_distribution_model, account_move_line, sale_order, sale_order_line
