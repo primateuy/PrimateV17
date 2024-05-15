88da923505d89147924b21ce9722fbed242466cf
LFer <lferreira@proyectasoft.com>
2024-05-15 16:43:18 -0300

    agregue un print

--- odoo_migrator/models/odoo_migrator.py
+++ odoo_migrator/models/odoo_migrator.py
@@ -1731,6 +1731,7 @@ class OdooMigrator(models.Model):
                 account_move_data["invoice_date"] = account_move_data.pop("date_invoice")
                 account_move_data["name"] = account_move_data.pop("number")
                 old_move_id_data = account_move_data.get('move_id', False)
+                print(old_move_id_data)
                 if old_move_id_data:
                     old_move_id = old_move_id_data[0]
                     account_move_data.pop("move_id")
